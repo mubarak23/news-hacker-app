e033493c49759e33c24878c251d5dc43cce23a86
mubarak23 <mubarakaminu340@gmail.com>
2023-10-15 06:19:52 +0100

    connect service with controller action

--- src/hacker-news/hacker-news.controller.ts
+++ src/hacker-news/hacker-news.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { HackerNewsService } from './hacker-news.service';
 
 @Controller('hacker-news')
-export class HackerNewsController {}
+export class HackerNewsController {
+  constructor(private readonly hackerNewsService: HackerNewsService) {}
+
+  @Get('top-25-words')
+  async getTop25Words(): Promise<string[]> {
+    const topWords = await this.hackerNewsService.getTop25Words();
+    return topWords;
+  }
+
+  @Get('last-week-words')
+  async getLastWeekWords(): Promise<string[]> {
+    const lastWeekWords = await this.hackerNewsService.getLastWeekWords();
+    return lastWeekWords;
+  }
+
+  @Get('top-600-words')
+  async getTop600Words(): Promise<string[]> {
+    const top600Words = await this.hackerNewsService.getTop600Words();
+    return top600Words;
+  }
+}
